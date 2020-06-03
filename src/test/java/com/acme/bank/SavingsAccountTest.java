5a09db8181e12ebcffcf8b2969097af0996a1e87
Billy <bkeefer55@gmail.com>
2020-06-03 21:37:23 +0100

    this is the first commit

---
+++ src/test/java/com/acme/bank/SavingsAccountTest.java
@@ -0,0 +1,20 @@
+package com.acme.bank;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SavingsAccountTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
